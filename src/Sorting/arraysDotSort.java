0053bfb958de1e0c394b52682283c877230d7c19
KetanBhoye <Imketanbhoye100@gmail.com>
2023-06-17 16:27:01 +0530

    Sorting

---
+++ src/Sorting/arraysDotSort.java
@@ -0,0 +1,15 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class arraysDotSort {
+    public static void main(String[] args){
+        int arr[] = {5,20,12,30};
+        char arr2[] ={'B','B','C','A','D'};
+
+        Arrays.sort(arr);
+    System.out.println(Arrays.toString(arr));
+    Arrays.sort(arr2);
+    System.out.println(Arrays.toString(arr2));
+    }
+}
---
+++ src/Sorting/bubbleSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+import java.util.Arrays;
+
+public class bubbleSort {
+    public static void main(String[] args){
+        int arr[] ={5,4,3,2,1};
+        boolean isSwapped = false;
+        for(int i=0;i<arr.length;i++){
+            for(int j =0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    isSwapped = true;
+                }
+            }
+            if(isSwapped==false){
+                break;
+            }
+        }
+    System.out.println(Arrays.toString(arr));
+    }
+
+}
